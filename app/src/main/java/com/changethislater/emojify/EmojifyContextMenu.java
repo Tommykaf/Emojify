cbf0d60014d9c85c7e811d1018a671d6dd104ff6
Tommy KA <poopmckaki@gmail.com>
2020-03-19 16:12:07 +0200

    added acttivity to text select context menu

--- app/src/main/java/com/changethislater/emojify/EmojifyContextMenu.java
+++ app/src/main/java/com/changethislater/emojify/EmojifyContextMenu.java
@@ -12,9 +12,28 @@ public class EmojifyContextMenu extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        handleText();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleText();
+    }
+
+    protected void handleText() {
         CharSequence text = getIntent()
                 .getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT);
-        Log.i("logtext",text.toString());
+        setContentView(R.layout.emojify_menu);
+        String res = text.toString().replace(" ", "\uD83D\uDC4F");
+        boolean readonly = getIntent()
+                .getBooleanExtra(Intent.EXTRA_PROCESS_TEXT_READONLY, false);
+
+        if (!readonly) {
+            Intent intent = new Intent();
+            intent.putExtra(Intent.EXTRA_PROCESS_TEXT, res);
+            setResult(RESULT_OK, intent);
+        }
     }
 
 }
