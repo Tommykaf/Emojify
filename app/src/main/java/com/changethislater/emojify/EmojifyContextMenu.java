49e3dde6ad36066509832fafcbb8eaa4bb545c99
Itamar Green <itamar.green@gmail.com>
2020-04-07 19:17:58 +0300

    fixed some stuff, added the replacement rules, and made the options in the list show the name of the replacement.

--- app/src/main/java/com/changethislater/emojify/EmojifyContextMenu.java
+++ app/src/main/java/com/changethislater/emojify/EmojifyContextMenu.java
@@ -1,32 +1,48 @@
 package com.changethislater.emojify;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.arch.core.util.Function;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class EmojifyContextMenu extends Activity {
 
     private LinearLayoutManager optionLayoutManager;
     private RecyclerView optionRecyclerView;
     private RecyclerView.Adapter optionListAdapter;
-    private List<String> optionList;
+    private TextView sampledTextView;
+    private Map<String, Function<String,String>> optionList;
     private CharSequence text;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        optionList = new ArrayList<>();
-        optionList.add("opt1");
-        optionList.add("opt2");
+        optionList = new HashMap<>();
+        optionList.put("\uD83D\uDC4F",(String s) -> s.replaceAll(" ","\uD83D\uDC4F"));
+        optionList.put("\uD83C\uDD71",(String s) -> s.replaceAll("nigg","ni\uD83C\uDD71\uD83C\uDD71"));
+        optionList.put("\uD83D\uDE02",(String s) -> {
+            String res = s.replaceAll("lol","\uD83D\uDE02");
+            res = res.replaceAll("LOL","\uD83D\uDE02");
+            return res;
+        });
+        optionList.put("\uD83E\uDD23",(String s) -> s.replaceAll("rofl","\uD83E\uDD23"));
         initView();
-        fetchText();
+        //fetchText();
     }
 
     @Override
@@ -37,15 +53,22 @@ public class EmojifyContextMenu extends Activity {
 
     private void initView() {
         setContentView(R.layout.emojify_menu);
+
         optionRecyclerView = findViewById(R.id.optionRecyclerView);
         optionRecyclerView.setHasFixedSize(true);
         optionLayoutManager = new LinearLayoutManager(this,RecyclerView.VERTICAL, false);
         optionRecyclerView.setLayoutManager(optionLayoutManager);
         optionListAdapter = new OptionAdapter(this.optionList);
         optionRecyclerView.setAdapter(optionListAdapter);
+
+        boolean fetched = fetchText();
+        if(fetched) {
+            sampledTextView = findViewById(R.id.sampledTextView);
+            sampledTextView.setText(text);
+        }
     }
 
-    protected void fetchText() {
+    protected boolean fetchText() {
         text = getIntent()
                 .getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT);
 
@@ -53,7 +76,13 @@ public class EmojifyContextMenu extends Activity {
                 .getBooleanExtra(Intent.EXTRA_PROCESS_TEXT_READONLY, false);
 
         if (readonly) {
-            //TODO: ADD TOAST
+            Context context = getApplicationContext();
+            CharSequence text = "Text is read only";
+            int duration = Toast.LENGTH_SHORT;
+            Toast.makeText(context, text, duration).show();
+            return false;
+        }else{
+            return true;
         }
     }
 
@@ -63,4 +92,14 @@ public class EmojifyContextMenu extends Activity {
         setResult(RESULT_OK, intent);
     }
 
+    public void OKButton(View view) {
+        String result = ((OptionAdapter) optionListAdapter).switcheroo(text);
+        //String result = ((OptionAdapter) Objects.requireNonNull(optionRecyclerView.getAdapter())).switcheroo(text);
+        Log.d("result",result);
+
+        Intent intent = new Intent();
+        intent.putExtra(Intent.EXTRA_PROCESS_TEXT, result);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
 }
--- app/src/main/java/com/changethislater/emojify/OptionAdapter.java
+++ app/src/main/java/com/changethislater/emojify/OptionAdapter.java
@@ -1,6 +1,7 @@
 package com.changethislater.emojify;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
@@ -8,17 +9,23 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.arch.core.util.Function;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class OptionAdapter extends RecyclerView.Adapter<OptionAdapter.OptionViewHolder> {
 
-    public List<String> options;
+    public Map<String, Function<String,String>> options;
+    private List<OptionViewHolder> viewHolders;
 
-
-    public OptionAdapter(List<String> options) {
+    public OptionAdapter(Map<String, Function<String,String>> options) {
         this.options = options;
+        this.viewHolders = new ArrayList<>();
     }
 
     @NonNull
@@ -28,17 +35,45 @@ public class OptionAdapter extends RecyclerView.Adapter<OptionAdapter.OptionView
         int layoutForItem = R.layout.option_view;
         LayoutInflater inflater = LayoutInflater.from(context);
         LinearLayout view = (LinearLayout) inflater.inflate(layoutForItem, parent, false);
-        return new OptionViewHolder(view);
+        OptionViewHolder viewHolder =  new OptionViewHolder(view);
+        viewHolders.add(viewHolder);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull OptionViewHolder holder, int position) {
-
+        Set<Map.Entry<String, Function<String,String>>> ress = options.entrySet();
+        int counter = 0;
+        Iterator<Map.Entry<String, Function<String,String>>> it = ress.iterator();
+        while(counter < position){
+            it.next();
+            counter++;
+        }
+        ((TextView) holder.view.findViewById(R.id.optText)).setText(it.next().getKey());
     }
 
     @Override
     public int getItemCount() {
         return options.size();
+    }
+
+    public String switcheroo(CharSequence input){
+        Log.d("yes123",input.toString());
+        List<Function<String,String>> operations = new ArrayList<>();
+        for(OptionViewHolder viewHolder : viewHolders){
+            if(viewHolder.isChecked()){
+                String name = (String) ((TextView)viewHolder.view.findViewById(R.id.optText)).getText();
+                if(options.containsKey(name)){
+                    operations.add(options.get(name));
+                }
+
+            }
+        }
+        String forFunc = input.toString();
+        for(Function<String,String> stringFunction : operations){
+            forFunc = stringFunction.apply(forFunc);
+        }
+        return forFunc;
     }
 
     class OptionViewHolder extends RecyclerView.ViewHolder {
@@ -50,11 +85,15 @@ public class OptionAdapter extends RecyclerView.Adapter<OptionAdapter.OptionView
             super(view);
             this.view = view;
             this.checkBox = view.findViewById(R.id.checkBox);
-            this.optionName = ((TextView) view.findViewById(R.id.textView)).getText().toString();
+            this.optionName = ((TextView) view.findViewById(R.id.optText)).getText().toString();
+
         }
 
         public boolean isChecked() {
             return this.checkBox.isChecked();
         }
+
+
+
     }
 }
